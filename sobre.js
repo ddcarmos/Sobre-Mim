5b6fbbc60fb4e7b5a74184ee8826630400391ef6
ddcarmos <daniele1b@hotmail.com>
2023-06-28 16:06:58 -0300

    sobre mim

---
+++ sobre.js
@@ -0,0 +1,11 @@
+// var n = document.getElementById('titlle');
+// n.addEventListener('mouseenter', mouseEnter);
+// // quando clicar nome o mesmo ficar azul // passar o mouse na saída  do nome  
+
+// //eventos intereção com elementos da pagina= mouse over e mouseout , click e dbclick evento de atualização 
+// function mouseEnter () {
+// n.innerText = "Pode me chamar: Dani"
+// n.style.color = 'pink'
+
+// }n
+
